c9c791716e7e4371fd04b8b48dd8c990eb022541
BHStoyanov23 <BHStoyanov23@codingburgas.bg>
2026-03-05 15:57:55 +0200

    Add main menu

--- Cinema/Cinema/Cinema.cpp
+++ Cinema/Cinema/Cinema.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
+#include "Menu.h"
+
 using namespace std;
 
+int main() {
+    int choice = -1;
 
-int main()
-{
-    
+    while (choice != 0) {
+        showMainMenu();
+        cin >> choice;
+
+        if (choice == 1) customerPortal();
+        else if (choice == 2) adminPortal();
+    }
+
+    cout << "Exiting system..." << endl;
+    return 0;
 }
-
---
+++ Cinema/Cinema/Menu.h
@@ -0,0 +1,10 @@
+#ifndef MENU_H
+#define MENU_H
+
+using namespace std;
+
+void showMainMenu();
+void customerPortal();
+void adminPortal();
+
+#endif
